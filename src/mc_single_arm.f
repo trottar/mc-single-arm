62c0a3b16820f9d795ee5354d65119be0fbb2db5
Richard Trotta <trottar@jlab.org>
2026-04-12 14:05:50 -0400

    Updated scripts to take number of good events as input with a cap

--- src/mc_single_arm.f
+++ src/mc_single_arm.f
@@ -1236,7 +1236,7 @@ C Close NTUPLE file.
       actual_generated_trials = armSTOP_trials
       if (actual_generated_trials.gt.0) then
          call normalize_ntuple_weights(hbook_filename,
-     >        actual_generated_trials)
+     >        actual_generated_trials + armSTOP_successes)
       endif
 
 		write (chanout,1002)
@@ -1250,7 +1250,7 @@ C Close NTUPLE file.
 		      write (chanout,1018)
 		   endif
 		endif
-      write (chanout,1019) actual_generated_trials
+      write (chanout,1019) actual_generated_trials + armSTOP_successes
 
 C Indicate where particles are lost in spectrometer.
 	if(ispec.eq.2) then
@@ -1387,7 +1387,7 @@ C =============================== Format Statements ============================
  1017 format(i11,' Target good events requested',/,
      >         i11,' Good events achieved')
  1018 format('WARNING: reached max generated trials before target good events.')
- 1019 format(i11,' Actual generated trials used to normalize weight/rate_hz')
+ 1019 format(i11,' Generated trials + successes used to normalize weight/rate_hz')
 
  1012 format(1x,16i4)
 
